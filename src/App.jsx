f67f2e3603d3e53e32050ce972320b81804f463b
shivam shrivastava <shivamshrivaastava@gmail.com>
2024-03-26 12:30:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import "./App.css";
 import SelectableGrid from "./components/selectable-grid";
+import CheckoutStepper from "./components/stepper";
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Customer Info",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
 function App() {
   return (
     <div>
       <h1>SelectableGrid</h1>
       <SelectableGrid rows={10} cols={10} />
+      <h2>Checkout</h2>
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPS} />
     </div>
   );
 }
